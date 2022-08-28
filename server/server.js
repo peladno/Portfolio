9171642a73e28b12271fbe51ff8bb6ff6af780c1
Javier Perez <j.perezurrutia@gmail.com>
2022-08-27 20:05:24 -0400

    back end mail done

--- server/server.js
+++ server/server.js
@@ -1,28 +1,33 @@
 import express from "express";
-import dotenv from "dotenv";
+import http from "http";
+import EmailSender from "./sendEmail.js";
+import { port, clientURL } from "./config.js";
+import cors from "cors";
 
-dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 8080;
+const httpServer = http.createServer(app);
+app.use(cors({ origin: `${clientURL}` }));
+const PORT = process.env.PORT || port;
 
 app.use(express.json());
-app.set("views", "./views");
-app.set("view engine", "ejs");
-
-//API
-app.post(
-  "/send",
-  async(req, (res) => {
-    try {
-      const { fullName, email, message } = req.body;
-      res.json({ msg: "server" });
-    } catch (error) {
-      res.status(404).json({ msg: error });
-    }
+app.use(
+  express.urlencoded({
+    extended: true,
   })
 );
 
-const server = app.listen(PORT, () => {
+//API
+app.post("/send", async (req, res) => {
+  try {
+    const { fullName, email, message } = req.body;
+    EmailSender({ fullName, email, message });
+    res.json({ msg: "Your message sent successfully" });
+  } catch (error) {
+    res.status(404).json({ msg: error });
+  }
+});
+
+httpServer.listen(PORT, () => {
   console.log(`Server http on ${PORT}...`);
 });
-server.on("error", (error) => console.log("Error on server", error));
+httpServer.on("error", (error) => console.log("Error on server", error));
