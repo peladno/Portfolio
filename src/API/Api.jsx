83cf624158e860158aae512a78848c50419601d4
Javier Perez <j.perezurrutia@gmail.com>
2022-08-31 00:41:33 -0400

    adding api url

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const SendEmail = async ({ fullName, email, message, setSend }) => {
   try {
     const datas = { fullName, email, message };
-    let res = await axios.post("http://localhost:8080/send", datas);
+    let res = await axios.post("https://portfoliojavierperez.herokuapp.com/send", datas);
     if (res) {
       setSend(res.data);
     }
