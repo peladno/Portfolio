ae7e48e34ecb162c691a8c9288e14fbcb01a1dbe
Javier Perez <j.perezurrutia@gmail.com>
2022-08-31 02:22:54 -0400

    api fix

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -6,8 +6,11 @@ export const SendEmail = async ({ fullName, email, message, setSend }) => {
     let res = await axios.post("https://portfoliojavierperez.herokuapp.com/send", datas);
     if (res) {
       setSend(res.data);
+      console.log(res.data);
+    } else {
+      console.log(res);
     }
   } catch (error) {
-    alert(error.response.data.msg);
+    console.log(error);
   }
 };
