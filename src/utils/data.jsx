5dc7953fdfa02b169f0cbd46387b1001d44cb1b1
Javier Perez <j.perezurrutia@gmail.com>
2022-09-04 20:32:23 -0400

    fixing imgs and adding animation to navbar desktop

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -5,7 +5,7 @@ export const portfolio = [
     description:
       "First project made for my class of web development. Simple static website for a existing club",
     skills: ["HTML", "Bootstrap", "CSS", "Sass"],
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/portfolioImages%2Fclubarduino.png?alt=media&token=0aa4eb7b-fba2-4384-ab85-5bfb0dd1faa9",
+    img: "/src/assets/images/portfolio/clubarduino.webp",
     web: "https://peladno.github.io/club-arduino/index.html",
     github: "https://github.com/peladno/club-arduino"
 
@@ -16,7 +16,7 @@ export const portfolio = [
     description:
       "Second project, for my class of JavaScript, Using almost all Vanilla JS and a bit of Bootstrap",
     skills: ["HTML", "CSS", "Sass", "JavaScript", "Bootstrap"],
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/portfolioImages%2Frecipes.png?alt=media&token=0b250f19-25c8-42b8-8f18-8726cdc56625",
+    img: "/src/assets/images/portfolio/recipes.webp",
     web: "https://peladno.github.io/JS28160JavierPerez/",
     github: "https://github.com/peladno/JS28160JavierPerez",
   },
@@ -26,7 +26,7 @@ export const portfolio = [
     description:
       "Third project for my class of React JS, using Firebase as a database (just to alocate products infomation)",
     skills: ["HTML", "CSS", "React", "Firebase", "MaterialUI"],
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/portfolioImages%2Feshop.png?alt=media&token=e9d9b764-d9dd-4da5-88fb-f03ad4c1d2f9",
+    img: "/src/assets/images/portfolio/eshop.webp",
     web: "https://javierperezreact31140.web.app/",
     github: "https://github.com/peladno/JavierPerezReact31140",
   },
@@ -35,8 +35,7 @@ export const portfolio = [
     name: "BackEnd challenges",
     description: "All the challenges I made for my class of BackEnd",
     skills: ["HTML", "CSS", "NodeJS", "Express", "MongoDB", "MySQL", "EJS"],
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/portfolioImages%2FCaptura%20desde%202022-08-30%2005-09-35.png?alt=media&token=70c24c93-0cf3-4df4-afde-60d3b87d0289",
-    web: "https://github.com/peladno/backendExpress",
+    img: "/src/assets/images/portfolio/challenge.webp",
     github: "https://github.com/peladno/backendExpress",
   },
   {
@@ -44,8 +43,7 @@ export const portfolio = [
     name: "Portfolio",
     description: "This portfolio",
     skills: ["HTML", "CSS", "Sass", "React", "NodeJS", "Express"],
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/portfolioImages%2FCaptura%20desde%202022-08-18%2001-01-32.png?alt=media&token=8955b3d8-d30a-43a9-9570-9ad51f133a93",
-    web: "https://portfoliojavierperez.web.app/",
+    img: "/src/assets/images/portfolio/portfolio.webp",
     github: "https://github.com/peladno/portfolio",
   },
 ];
@@ -53,23 +51,23 @@ export const portfolio = [
 export const skills = [
   {
     name: "HTML",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2Fhtml.svg?alt=media&token=0a49a966-9544-48f3-9d96-9762eabf7350",
+    img: "/src/assets/images/svgSkills/html.svg",
   },
   {
     name: "CSS",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2Fcss.svg?alt=media&token=206574fc-f491-49f0-aec2-7cc1675a1467",
+    img: "/src/assets/images/svgSkills/css.svg",
   },
   {
     name: "JavaScript",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2Fjs.svg?alt=media&token=646fc495-116b-4f74-95d2-14656610b176",
+    img: "/src/assets/images/svgSkills/js.svg",
   },
   {
     name: "React",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2Freact.svg?alt=media&token=3c7538b8-ab1d-49d3-8c1b-24a56b46050c",
+    img: "/src/assets/images/svgSkills/react.svg",
   },
   {
     name: "NodeJS",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2FVector.svg?alt=media&token=6f4d847d-f371-411f-a3a6-acffe972e81d",
+    img: "/src/assets/images/svgSkills/Vector.svg",
   },
   {
     name: "Express",
@@ -77,35 +75,35 @@ export const skills = [
   },
   {
     name: "MongoDB",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2Fmongo.svg?alt=media&token=83e9c46a-b5e6-473e-bfdd-bfc937fda353",
+    img: "/src/assets/images/svgSkills/mongo.svg",
   },
   {
     name: "Git",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2Fgit.svg?alt=media&token=650f8318-d5fa-4e4c-87db-886461261815",
+    img: "/src/assets/images/svgSkills/git.svg",
   },
   {
     name: "Sass",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2Fsass.svg?alt=media&token=d49f8dda-5b2e-40e5-aac8-1f20c384afb7",
+    img: "/src/assets/images/svgSkills/sass.svg",
   },
   {
     name: "Bootstrap",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2Fbootstrap.svg?alt=media&token=3b3afac4-0341-4d22-92af-64d391ae75b0",
+    img: "/src/assets/images/svgSkills/bootstrap.svg",
   },
   {
     name: "EJS",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2Fejs.svg?alt=media&token=ad8720ff-92f5-417a-9661-b546b1f12725",
+    img: "/src/assets/images/svgSkills/ejs.svg",
   },
   {
     name: "Firebase",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2Ffirebase.svg?alt=media&token=b3e5af6d-a753-41c2-ae2b-e3f95fa66294",
+    img: "/src/assets/images/svgSkills/firebase.svg",
   },
   {
     name: "MaterialUI",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2FmaterialUI.svg?alt=media&token=2f21ca32-9ca1-4785-81b7-2844f9ffa012",
+    img: "/src/assets/images/svgSkills/materialUI.svg",
   },
   {
     name: "MySQL",
-    img: "https://firebasestorage.googleapis.com/v0/b/portfoliojavierperez.appspot.com/o/iconsSVG%2FmySql.svg?alt=media&token=815c35d3-fa9f-43d2-9ba2-555ba98e42ea",
+    img: "/src/assets/images/svgSkills/mySql.svg",
   },
 ];
 
