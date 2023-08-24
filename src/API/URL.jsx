71a16c9afecfbbb25382bc87f2d4727237ba1760
Javier <j.perezurrutia@gmail.com>
2023-08-25 01:33:26 +0900

    adding amplitude

--- src/API/URL.jsx
+++ src/API/URL.jsx
@@ -1,4 +1,5 @@
-export const { CV_URL, API_MAIL } = {
+export const { CV_URL, API_MAIL, AMPLITUDE } = {
   CV_URL: import.meta.env.VITE_BASE_URL,
   API_MAIL: import.meta.env.VITE_API,
+  AMPLITUDE: import.meta.env.VITE_AMPLITUDE,
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Index from "./pages/Index";
-import "./styles/styles.scss";
-import WebRoutes from "./components/WebRoutes";
-import { DarkModeProvider } from "./context/ThemeContext";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Index from './pages/Index';
+import './styles/styles.scss';
+import WebRoutes from './components/WebRoutes';
+import { DarkModeProvider } from './context/ThemeContext';
+import * as amplitude from '@amplitude/analytics-browser';
+import { AMPLITUDE } from './API/URL';
 
 function App() {
+  amplitude.init(AMPLITUDE);
+
   return (
     <>
       <DarkModeProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/*" element={<WebRoutes />} />
+            <Route path='/' element={<Index />} />
+            <Route path='/*' element={<WebRoutes />} />
           </Routes>
         </BrowserRouter>
       </DarkModeProvider>
